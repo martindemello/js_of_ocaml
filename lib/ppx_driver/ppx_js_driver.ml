e797c6121139e579a3f1406f03a5fd9382154257
Hugo Heuzard <hugo.heuzard@gmail.com>
2017-01-27 11:49:03 +0000

    fix for 4.05

--- lib/ppx_driver/ppx_js_driver.ml
+++ lib/ppx_driver/ppx_js_driver.ml
@@ -3,6 +3,22 @@ let () =
     (Arg.Unit (fun () -> Ppx_js.wrapper := None))
     ~doc:" Undocumented";
   let js_mapper = Ppx_js.js_mapper [] in
+  let module Js = Ppx_ast.Selected_ast in
+  let module Ocaml = Migrate_parsetree.Ast_current in
   Ppx_driver.register_transformation "js_of_ocaml"
-    ~impl:(js_mapper.Ast_mapper.structure js_mapper)
-    ~intf:(js_mapper.Ast_mapper.signature js_mapper)
+    ~impl:(fun st ->
+      Js.ast_of_impl st
+      |> Js.to_ocaml_ast
+      |> Ocaml.impl_of_ast
+      |> js_mapper.Ast_mapper.structure js_mapper
+      |> Ocaml.ast_of_impl
+      |> Js.of_ocaml_ast
+      |> Js.impl_of_ast)
+    ~intf:(fun sg ->
+      Js.ast_of_intf sg
+      |> Js.to_ocaml_ast
+      |> Ocaml.intf_of_ast
+      |> js_mapper.Ast_mapper.signature js_mapper
+      |> Ocaml.ast_of_intf
+      |> Js.of_ocaml_ast
+      |> Js.intf_of_ast)
